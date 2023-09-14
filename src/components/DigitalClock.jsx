398cb52db9bb935b840ce981fcbaf986724c9b93
Ayoub benouahi <93976857+Benouahi1@users.noreply.github.com>
2023-09-14 21:47:48 +0100

    ayoub

---
+++ src/components/DigitalClock.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import { AlarmContext } from '../context/Alarm';
+import './digitalClock.css';
+import SandClockSpinner from '../Loader/SandClock';
+
+const DigitalClock = () => {
+  const {
+    hourDigital,
+    minutesDigital,
+    amPm,
+    dayNow,
+    monthNow,
+    yearNow,
+    showSpinner,
+  } = useContext(AlarmContext);
+
+  console.log(showSpinner);
+
+  return (
+    <div>
+      <div className="clock-text">
+        {showSpinner && <SandClockSpinner />}
+        <div className="clock-text-hour">{`${hourDigital}:`}</div>
+        <div className="clock-text-minutes">{minutesDigital}</div>
+        <div className="clock-text-ampm">{amPm}</div>
+      </div>
+      <div className="clock-date">
+        <span>{`${dayNow} `}</span>
+        <span>{`${monthNow} , `}</span>
+        <span>{yearNow}</span>
+      </div>
+    </div>
+  );
+};
+
+export default DigitalClock;
